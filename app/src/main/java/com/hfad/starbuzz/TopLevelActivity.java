6e6de57fd8beefd7c579f946a1f18fbdedafed8f
Jon Gracias <jonpgracias@gmail.com>
2021-03-24 15:59:43 -0400

    added strings.xml array_list and starbuzz-logo, added DrinkCategoryActivity.java

--- app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
@@ -2,7 +2,12 @@ package com.hfad.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        //Create an onItemClickListener
+        AdapterView.OnItemClickListener itemClickListener =
+                (listView, itemView, position, id) -> {
+                    if(position == 0) {
+                        Intent intent = new Intent(TopLevelActivity.this,
+                                                   DrinkCategoryActivity.class);
+                        startActivity(intent);
+                    }
+                };
+        //Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
