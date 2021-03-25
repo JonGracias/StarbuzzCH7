501b2bfb296f31b279dbd6ade0d257708d0031ce
Jon Gracias <jonpgracias@gmail.com>
2021-03-25 10:44:53 -0400

    added DrinkCategoryActivity.java and activity_drink_category.xml

--- app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -1,8 +1,11 @@
 package com.hfad.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+//imported classes
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class DrinkCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+
+        //ArrayAdapter so listView can show drink names
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
+
+        //Create the listener
+
+
     }
 }
